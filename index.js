2cd073d7e85763d22372615e2e2d756175de712c
John Tomaselli <jrt@gtz.com>
2024-11-12 22:51:56 -0500

    Update index.js

--- index.js
+++ index.js
@@ -6,13 +6,15 @@ fastify.register(require('@fastify/formbody'));
 
 const backendData = {};
 let sessionid = 0;
- //<button data-on-click="$$put('/sessionid')">Send Session</button> </br>
+let session_id = 0;
+//<button data-on-click="$$put('/sessionid')">Send Session</button> </br>
 function indexPage() {
   // this only gets set the first time user hits the page
   sessionid++
+  session_id++
   const indexPage = `
-    <!doctype html><html>
-   
+    <!doctype html><html>   
+
       <head>
         <title>Node/Fastify + Datastar Example With Anonymous User Local Storage</title>
         <script type="module" defer src="https://cdn.jsdelivr.net/npm/@sudodevnull/datastar"></script></head>
@@ -20,7 +22,7 @@ function indexPage() {
         <h2>Node/Fastify + Datastar Example</h2>
         <main class="container" id="main" data-store='{ input: "", show: false }'>
         <div id="result"></div>
-        <p id="name">Hello</p>
+         <div id="name"></div>
         </br>
         <script>
         var foo = new Set();
@@ -30,32 +32,29 @@ function indexPage() {
         
         localStorage.setItem("set",JSON.stringify([...foo]));
         var x = localStorage.getItem('set');
-       // console.log('x: ', JSON.parse(x));
         function setPerson(){
-          // var person2 = { 'name': document.getElementById('name').innerHTML}
-          var person = {
-            // name: document.getElementById('name').value, FAILS
-            name: ${sessionid}
+          var person${session_id} = {
+            name: ${session_id}
            };
       
           // Put the object into storage 
-          localStorage.setItem('person', JSON.stringify(person));
+          localStorage.setItem('person${session_id}', JSON.stringify(person${session_id}));
           }
           setPerson()
           
           // Retrieve
-          let obj = localStorage.getItem("person")
+          let obj = localStorage.getItem("person${session_id}")
           obj = JSON.parse(obj);
           console.log(obj)
           let na = obj.name
-          console.log(na)
-          document.getElementById("result").innerHTML = na
-          // .name nor .value
-          //localStorage.getItem("person").name;
-       function getPerson(){
-        let name = JSON.parse(localStorage.getItem("person")).name
-        console.log('getname ',name)
-        ;
+          console.log('obj.name na = ',na)
+         // alert(na)
+          document.getElementById("result").innerHTML = "Hello  ${session_id} "
+          
+           function getPerson(){
+        let name = JSON.parse(localStorage.getItem("person${session_id}")).name
+          
+        console.log('getname ',name)     ;
        }
        getPerson()
         </script>
@@ -76,10 +75,26 @@ function indexPage() {
           <span>Feed from server: </span>
           <span id="feed" data-on-load="$$get('/feed')"></span>
         </div>
+
+        <!-- Removed styling /examples -->
+<div id="contact_1">
+  <label>First Name: John</label>
+  <label>Last Name: Doe</label>
+  <label>Email: joe@blow.com</label>
+  <div>
+    <button data-on-click="$$get('/click_to_edit/contact/1/edit')">
+      Edit
+    </button>
+    <button data-on-click="$$get('/click_to_edit/contact/1/reset')">
+      Reset
+    </button>
+  </div>
+</div>
       </body>
     </html>`;
   return indexPage;
 }
+
 
 function setSSEHeaders(reply) {
   reply.raw.setHeader('Cache-Control', 'no-cache');
@@ -117,7 +132,7 @@ fastify.put('/put', async (request, reply) => {
   backendData.input = input;
   const output = `Your input: ${input}, is ${input.length} long. sessionid = ${sessionid} `;
   const frag = `<div id="output">${output}</div>`;
-  
+
   sendSSE({
     reply,
     frag,
@@ -126,7 +141,7 @@ fastify.put('/put', async (request, reply) => {
     mergeType: 'morph',
     end: true
   });
-  
+
   // We need to return undefined to prevent Fastify from automatically ending the response
   return undefined;
 });
@@ -139,8 +154,8 @@ fastify.put('/put/:session_id', async (request, reply) => {
   backendData.input = input;
   // const output = `Your input: ${input}, is ${input.length} long. sessionid = ${sessionid} `;
   const output = `Your input: ${input}, is ${input.length} long. sessionid = ${session_id} `;
- const frag = `<div id="output">${output}</div>`;
-  
+  const frag = `<div id="output">${output}</div>`;
+
   sendSSE({
     reply,
     frag,
@@ -149,17 +164,17 @@ fastify.put('/put/:session_id', async (request, reply) => {
     mergeType: 'morph',
     end: true
   });
-  
+
   // We need to return undefined to prevent Fastify from automatically ending the response
   return undefined;
 });
 // fastify.get('/get', async (request, reply) => {
-  fastify.get('/get/:session_id', async (request, reply) => {
-    const {
-      session_id
-    } = request.params;
-    setSSEHeaders(reply);
-  
+fastify.get('/get/:session_id', async (request, reply) => {
+  const {
+    session_id
+  } = request.params;
+  setSSEHeaders(reply);
+
   const output = `Backend State: ${JSON.stringify(backendData)}.`;
   let frag = `<div id="output2">${output} sessionid = ${session_id}</div>`;
 
@@ -177,13 +192,15 @@ fastify.put('/put/:session_id', async (request, reply) => {
 
 fastify.get('/feed', async (request, reply) => {
   setSSEHeaders(reply);
-  
+
   // Create an async iterator for the SSE feed
   const feedHandler = async () => {
     while (!reply.raw.destroyed) {
       const rand = randomBytes(8).toString('hex');
-      const frag = `<span id="feed">${rand}</span>`;
-      
+      // const frag = `<span id="feed">${rand}</span>`;
+      const output = `Backend State: ${JSON.stringify(backendData)}.`;
+      let frag = `<div id="output2">${output} sessionid = ${session_id} ${rand}</div>`;
+
       sendSSE({
         reply,
         frag,
@@ -208,12 +225,42 @@ fastify.get('/feed', async (request, reply) => {
   return undefined;
 });
 
+// fastify.get('/click_to_edit/contact/1/edit', async (request, reply) => {
+//   return formTemplate(contact);
+// });
+// fastify.get('/click_to_edit/contact/1/edit', async (request, reply) => {
+//   return formTemplate(contact);
+// });
+
+// fastify.get('/click_to_edit/contact/1/reset', async (request, reply) => {
+
+// });
+// function formTemplate(contact) {
+//   return `<form hx-put="/contact/1" hx-target="this" hx-swap="outerHTML">
+//   <div>
+//   <label>First Name</label>
+//   <input type="text" name="firstName" value="${contact.firstName}">
+//   </div>
+//   <div class="form-group">
+//   <label>Last Name</label>
+//   <input type="text" name="lastName" value="${contact.lastName}">
+//   </div>
+//   <div class="form-group">
+//   <label>Email Address</label>
+//   <input type="email" name="email" value="${contact.email}">
+//   </div>
+//   <button class="btn">Submit</button>
+//   <button class="btn" hx-get="/contact/1">Cancel</button>
+//   </form>`
+//   }
 // Start the server
 const start = async () => {
   try {
     const PORT = process.env.PORT || 3000;
     await fastify.listen({ port: PORT });
     fastify.log.info(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}`);
+
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
